4b245038ef783a33a6c241afaa5637019761adf3
Aleksei Belov <aleksei.belov@wisetechglobal.com>
2025-04-16 15:37:44 +1000

    Added second test projects

---
+++ DependabotTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		// Create a person object
+		Person person = new Person
+		{
+			Name = "Alice",
+			Age = 30
+		};
+
+		// Serialize the person object to JSON
+		string json = JsonConvert.SerializeObject(person);
+		Console.WriteLine("Serialized JSON:\n" + json);
+
+		// Deserialize the JSON back to a person object
+		Person deserializedPerson = JsonConvert.DeserializeObject<Person>(json);
+		Console.WriteLine("\nDeserialized Object:");
+		Console.WriteLine($"Name: {deserializedPerson.Name}, Age: {deserializedPerson.Age}");
+	}
+}
+
+public class Person
+{
+	public string Name { get; set; }
+	public int Age { get; set; }
+}
---
+++ TestProject2/TestProject2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using Snappier;
+
+class Program
+{
+	static void Main()
+	{
+		string originalText = "Hello, this is a test string to compress using Snappy!";
+		Console.WriteLine("Original: " + originalText);
+
+		// Convert string to byte array
+		byte[] inputBytes = Encoding.UTF8.GetBytes(originalText);
+
+		// Compress
+		int compressed = Snappy.Compress(input: inputBytes, output: new Span<byte>(new byte[Snappy.GetMaxCompressedLength(inputBytes.Length)]));
+		Console.WriteLine("Compressed size: " + compressed);
+	}
+}
